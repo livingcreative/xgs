313eaee5681472ce5fa70fffd6ff547ae121c6a5
livingcreative <dnkreative@gmail.com>
2016-07-13 18:19:47 +0500

    Windows platform dynamic library sources

---
+++ src/opengl/win/xGSOpenGL.cpp
@@ -0,0 +1,24 @@
+﻿/*
+        xGS 3D Low-level rendering API
+
+    Low-level 3D rendering wrapper API with multiple back-end support
+
+    (c) livingcreative, 2015 - 2016
+
+    https://github.com/livingcreative/xgs
+
+    opengl/win/xGSOpenGL.cpp
+        "main" source for xGS DLL on Windows
+*/
+
+#include "xGS/xGS.h"
+
+
+extern "C" {
+
+    GSbool xGSAPI xGSCreate(IxGS *xgs)
+    {
+        return Create(xgs);
+    }
+
+}
