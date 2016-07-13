0f30b6d0a0ce13649ec5932038ecc11f194be091
livingcreative <dnkreative@gmail.com>
2016-07-13 18:20:29 +0500

    cmake project sources (1st version, windows build supported)

---
+++ src/opengl/OpenGL.cmake
@@ -0,0 +1,64 @@
+﻿#
+#      xGS 3D Low-level rendering API
+#
+#  Low-level 3D rendering wrapper API with multiple back-end support
+#
+#  (c) livingcreative, 2015 - 2016
+#
+#  https://github.com/livingcreative/xgs
+#
+#  opengl/OpenGL.cmake
+#      cmake include file for OpenGL implementation build
+#
+
+# OpenGL implementation specific headers
+set(HEADERS
+	${HEADERS}
+	opengl/xGScontext.h
+	opengl/xGSdatabuffer.h
+	opengl/xGSframebuffer.h
+	opengl/xGSgeometry.h
+	opengl/xGSgeometrybuffer.h
+	opengl/xGSGLutil.h
+	opengl/xGSimpl.h
+	opengl/xGSinput.h
+	opengl/xGSparameters.h
+	opengl/xGSstate.h
+	opengl/xGStexture.h
+)
+
+# OpenGL implementation specific sources
+set(SOURCES
+	${SOURCES}
+	opengl/xGSmain.cpp
+
+	# xGSdatabuffer.cpp
+	# xGSframebuffer.cpp
+	# xGSgeometry.cpp
+	# xGSgeometrybuffer.cpp
+	# xGSGLutil.cpp
+	# xGSimpl.cpp
+	# xGSinput.cpp
+	# xGSparameters.cpp
+	# xGSstate.cpp
+	# xGStexture.cpp
+)
+
+# OpenGL implementation additional include dirs
+include_directories(opengl/..)
+include_directories(opengl)
+
+# OpenGL implementation libs for shared library
+set(SHAREDLIB_LIBS
+	xGSOpenGLLib
+)
+
+# Windows specific headers and sources
+if (WIN32)
+	include(opengl/win/windows.cmake)
+	set(PLATFORMPROP WIN32)
+endif ()
+
+# OpenGL implementation target names
+set(xGSStaticLib xGSOpenGLLib)
+set(xGSSharedLib xGSOpenGL)
---
+++ src/opengl/win/windows.cmake
@@ -0,0 +1,43 @@
+﻿#
+#      xGS 3D Low-level rendering API
+#
+#  Low-level 3D rendering wrapper API with multiple back-end support
+#
+#  (c) livingcreative, 2015 - 2016
+#
+#  https://github.com/livingcreative/xgs
+#
+#  opengl/win/windows.cmake
+#      cmake include file for OpenGL Windows implementation
+#
+
+# windows platform build headers
+set(HEADERS
+	${HEADERS}
+
+	opengl/win/glplatform.h
+	opengl/win/xGScontextplatform.h
+	opengl/win/xGSdefaultcontext.h
+)
+
+# additional include dirs for build
+include_directories(opengl/win)
+
+# On Windows OpenGL implementation depends on GLEW library
+if (NOT GLEW)
+	set(GLEW ${PROJECT_SOURCE_DIR}/../../glew)
+endif ()
+include_directories(${GLEW}/include)
+link_directories(${GLEW}/lib)
+add_definitions(-DGLEW_STATIC)
+set(SHAREDLIB_LIBS
+	${SHAREDLIB_LIBS}
+	opengl32
+	glew32s
+)
+
+# Windows shared library source files
+set(SHARED_SOURCES
+	opengl/win/xGSOpenGL.cpp
+	opengl/win/xGSOpenGL.def
+)
